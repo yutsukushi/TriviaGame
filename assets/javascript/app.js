f81c7c68451bd43598a938d1718bb71019bb8bf5
yutsukushi <ykushibi@gmail.com>
2019-05-27 19:33:28 -0400

    functioning answer buttons, new question and timer display after every user click

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,6 +1,9 @@
 // GLOBAL VARIABLES
 
-var timer = 30;
+var allowedTime = 30;
+
+
+var timer = allowedTime;
 var countdown;
 var nextQTimer;
 var correct = 0;
@@ -8,8 +11,8 @@ var wrong = 0;
 var unanswered = 0;
 
 var pTag = $("p");
-var btn = $("button");
-var question = $(".question");
+var questionHTML = $(".question");
+var answerHTML = $(".answers");
 
 var displayTimer = $(".displayTimer");
 
@@ -28,7 +31,8 @@ displayQuestions();
 });
 
 function timerStart() { //timer countdown decrements by 1 unit per second,
-
+   timer = allowedTime;
+   clearInterval(countdown);
    countdown = setInterval(timerCountdown, 1000);
    timerCountdown();
 
@@ -48,7 +52,7 @@ function timerCountdown() {
 }
 
 
-function displayQuestions() {
+function displayQuestions() { // loops through all objects in questionObjs
 
     //set up an object with questions and the corresponding answers, and display a question at a time.
     //set up a variable with the index 
@@ -56,11 +60,9 @@ function displayQuestions() {
     //move onto next question after alotted time.
     // increment index so it chooses the next question in object array
 
-    i = 0;
-
-    questionObj = [{
+    questionObjs = [{
     q: "What is the only U.S. state that borders another state?",
-    correct: "B: Maine",
+    correct: "b",
     answers: {
         a: "A: Rhode Island",
         b: "B: Maine", //Maine only shares a border with New Hampshire
@@ -70,6 +72,7 @@ function displayQuestions() {
        
 }, {
     q: "Name the number that is three more than one-fifth of one-tenth of one-half of 5,000",
+    correct: "c",
     answers: {
         a: "A: " + 503,
         b: "B: " + 103,
@@ -78,6 +81,7 @@ function displayQuestions() {
     }
 }, {
     q: "1, 1, 2, 3, 5, 8, 13, _, 34: What's the missing number?",
+    correct: "b",
     answers: {
         a: "A: " + 20,
         b: "B: " + 21, //answer
@@ -86,6 +90,7 @@ function displayQuestions() {
     }
 }, {
     q: "What's the oldest continuously inhabited city in the world?",
+    correct: "d",
     answers: {
         a: "A: Istanbul, Turky",
         b: "B: Athens, Greece",
@@ -94,6 +99,7 @@ function displayQuestions() {
     }
 }, {
     q: "Which of these cars did James Bond not drive in any of the James Bond films?",
+    correct: "c",
     answers: {
         a: "A: Bentley",
         b: "B: Toyota",
@@ -102,69 +108,10 @@ function displayQuestions() {
     }
 }];
 
-    question.html(questionObj[i].q); //successfully displays question on screen
-    
-    var answerArr = (Object.values(questionObj[i].answers)); //returns object values into an array
+var i = 0;
 
-    for (var i = 0; i < answerArr.length; i++) { //for loop to create a button for every answer
-        
-        btnCreate = $('<br><br><button type="button" class="btn btn-info answerBtn">' + answerArr[i] + '</button><br>');
-        
-        // btnCreate.appendTo(pTag); 
-        pTag.append(btnCreate);
-    }
-
-    var answer = $(".answerBtn");
-
-    console.log(btnCreate === questionObj[i].correct);
-
-    pTag.on("click", ".answerBtn", function(){ //delegated functions specifically for anwer buttons when they are created.
-
-        if (btnCreate === questionObj[i].correct) { //if the button clicked has a value, you're right!
-    
-        //display the correct result
-        //"Correct!"
-        //after 15s, move onto next question
-
-        correct++;
-        question.hide();
-        answer.hide();
-        displayTimer.hide();
-
-        pTag.append("You're correct!");
-        answerReveal(); 
-        console.log("right: " + correct);
-
-        } else if (displayTimer === 0) {
-            // display the correct result
-            // //"Ran out of time! The correct answer is "
-            // // after 15s, move onto next question
-
-            unanswered++;
-            question.hide();
-            answer.hide();
-            displayTimer.hide();
-        
-            pTag.append("Ran out of time! The correct answer is " + questionObj[i].correct);
-            answerReveal(); 
-            console.log("unanswered: " + unanswered);
-            
-        } else {
-
-            wrong++;
-            question.hide();
-            answer.hide();
-            displayTimer.hide();
-
-            pTag.append("Oops! The correct answer is " + questionObj[i].correct);
-            answerReveal();
-            console.log("wrong: " + wrong);
-        
-        }
-        
-        console.log("it worked");
-        
-    });
+displayQuestion(questionObjs[i], answerClickHandler);
+    // wait for the next question
 
     // if (displayTimer === 0) { //if timer hits 0, You're wrong!
     
@@ -177,33 +124,84 @@ function displayQuestions() {
     // displayTimer.hide();
 
     // unanswered++;
-    // pTag.append("Ran out of time! The correct answer is " + questionObj[i].correct);
+    // pTag.append("Ran out of time! The correct answer is " + questionObjs[i].correct);
     // console.log("wrong: " + wrong);
     
     // }
+
+    function answerClickHandler() {
+        console.log(this.id);
+        clearQuestion();
+        if (this.id === questionObjs[i].correct) { //if the button clicked has a value, you're right!
+            //display the correct result
+            //"Correct!"
+            //after 15s, move onto next question
+            correct++;
+
+            pTag.append("You're correct!");
+            console.log("right: " + correct);
+
+        // } else if (displayTimer === 0) {
+        //     // display the correct result
+        //     // //"Ran out of time! The correct answer is "
+        //     // // after 15s, move onto next question
+
+        //     unanswered++;
+        //     question.hide();
+        //     answer.hide();
+        //     displayTimer.hide();
+        
+        //     pTag.append("Ran out of time! The correct answer is " + questionObjs[i].correct);
+        //     answerReveal(); 
+        //     console.log("unanswered: " + unanswered);
+            
+        } else {
+            wrong++;
+            pTag.append("Oops! The correct answer is " + questionObjs[i].correct);
+            //answerReveal();
+            console.log("wrong: " + wrong);
+        
+        }
+        setTimeout(function(){
+            i++;
+            pTag.empty();
+            if (i >= questionObjs.length) {
+                pTag.append("This is how you did...<br>Correct: " + correct + "<br>Incorrect: " + wrong + "<br>Unanswered: " + unanswered);
+                pTag.append($('<br><br><button type="button" class="btn btn-info" id="tryAgain">' + "Try again?" + '</button><br>'));
+                
+                tryAgain();
+            } else {
+                displayQuestion(questionObjs[i], answerClickHandler);
+            }
+        }, 1000);
+    }
     
-};
-
-function answerReveal() {
- nextQTimer = setTimeout(nextQuestion, 1000 * 10); //after 10s, display next question and answers
 }
 
-function nextQuestion() { //display next question and answers
-    // i++ for the next question
-    // question.show(questionObj[i].q)
+function displayQuestion(questionObj, clickHdlr) {
+    timerStart();
+    displayTimer.show();
+    questionHTML.html(questionObj.q); //successfully displays question on screen
 
+    for (var questionLetter in questionObj.answers) { //for loop to create a button for every answer
+        var answerText = questionObj.answers[questionLetter];
+        btn = $('<br><br><button type="button" class="btn btn-info answerBtn" id="' + questionLetter + '">' + answerText + '</button><br>');
+        
+        btn.on("click", clickHdlr);
+        answerHTML.append(btn);
+    }
+    console.log(answerHTML);   
 }
 
-function displayResults() {
-//when questions are all answered
-// # of questions answered correctly, from displayFourAnswers
-// # of questions answered wrong, from displayFourAnswers
-// display start over button
+function clearQuestion() {
+    questionHTML.empty();
+    answerHTML.empty();
+    displayTimer.hide();
 }
 
-function startOver() {
-// when clicked remove contents of page
-// displayQuestions() function
+function tryAgain() {
+    timerStart();
+    displayQuestions();
 
 }
 
